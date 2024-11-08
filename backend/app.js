39749b22f618e4fbdc1ef9fb449ac89fe36178c4
kanwararpit <arpit.kanwar@quarkifi.com>
2024-11-08 14:57:13 +0530

    add browser config

--- backend/app.js
+++ backend/app.js
@@ -7,21 +7,14 @@ import router from './routes/userAuthRoute.js'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-
-// Load environment variables
 dotenv.config({ path: './config/config.env' })
 
-// Define __filename and __dirname
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-// Log the static path
 export const staticPath = path.join(__dirname, 'public')
-const new__dirname = path.resolve()
-console.log("the new dirname>>>>",new__dirname);
 const app = express()
 
-// Serve static files from the "public" directory
 app.use(express.static(staticPath))
 
 app.set('view engine', 'jsx')
@@ -33,7 +26,6 @@ app.use(express.urlencoded({ extended: true }))
 // API routes
 app.use('/api/v4/user', router)
 
-// Database connection
 databaseConnection()
 
 export default app
