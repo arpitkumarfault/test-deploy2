cad44073c5913a69769c27e794132d87be25465a
arpit kanwar <168622732+kanwarAnshul@users.noreply.github.com>
2024-11-08 15:05:13 +0530

    Update vite.config.js
    add new deploy url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://task-management-backend-yfrd.onrender.com',
+      '/api': 'https://task-management-new-backend.onrender.com',
     }
   },
   plugins: [react()],
